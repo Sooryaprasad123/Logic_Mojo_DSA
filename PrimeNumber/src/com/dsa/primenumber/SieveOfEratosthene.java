66ac4ace7751797f1cfc7cf2da88e451099766d7
suryaprasad <suryaprasad@192.168.0.107>
2025-09-10 14:42:52 +0530

    prime number - sieve of eratosthene approach

---
+++ PrimeNumber/src/com/dsa/primenumber/SieveOfEratosthene.java
@@ -0,0 +1,34 @@
+package com.dsa.primenumber;
+
+public class SieveOfEratosthene {
+
+	public static void IsPrime(int n) {
+
+		boolean isPrime[] = new boolean[n + 1];
+
+		for (int i = 2; i <= n; i++) {
+			isPrime[i] = true;
+		}
+
+		for (int i = 2; i <=n; i++) {
+			if (isPrime[i]) {
+				for (int p = i * i; p <=n; p += i) {
+					isPrime[p] = false;
+				}
+
+			}
+		}
+
+		for (int i = 2; i <= n; i++) {
+			if (isPrime[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		int n = 15;
+		IsPrime(n);
+	}
+
+}
