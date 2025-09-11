a4031380cbe16c2ae1611494723ecefa349d8c4a
suryaprasad <suryaprasad@192.168.0.101>
2025-09-11 13:01:00 +0530

    Gcd of given 2 numbers using Euclids algorithm

---
+++ GCD/src/com/basic/gcd/GCD.java
@@ -0,0 +1,23 @@
+package com.basic.gcd;
+
+public class GCD {
+
+	public static int euclidAlgo(int a, int b) {
+		while (b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+
+		}
+		return a;
+
+	}
+
+	public static void main(String args[]) {
+		int a = 36;
+		int b = 60;
+		int result = euclidAlgo(a, b);
+		System.out.println("Gcd of " + a + "," + b + " is " + result);
+	}
+
+}
